d25000cc5e8c1dcd6d5217f7a06694dcc5666512
BaUka0 <bauirzhan.bisanov@gmail.com>
2025-04-27 12:12:30 +0500

    а

---
+++ CRMSystem.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRMSystem.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
